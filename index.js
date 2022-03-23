5625bdb9ffa2c680ba9cdd491d94992e842ea67f
Jaime Montes de Oca <jaime0422@live.com>
2022-03-22 20:25:26 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+
+function saturdayFun(action = "roller-skate") {
+    return `This Saturday, I want to ${action}!`;
+}
+
+const mondayWork = function (action = "go to the office") {
+    return `This Monday, I will ${action}.`;
+}
+
+function wrapAdjective(visualFlair = "*") {
+    return function(message = "special") {
+        return `You are ${visualFlair}${message}${visualFlair}!`;
+    }
+}
+
